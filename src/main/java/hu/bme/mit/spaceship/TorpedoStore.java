011be8438ff92d83cb37539fb4ab26f3fb896066
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2024-10-16 08:44:47 +0200

    Added requested comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -29,7 +29,7 @@ public class TorpedoStore {
       }
     }
   }
-
+  // Function to fire the torepdos
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
